661dc2c0ce2c1c09b3d51a71042981b5968076fd
nguyenhuunhon <126145765+NhonnNguyenn@users.noreply.github.com>
2025-09-21 19:36:07 +0700

    Delete src directory

--- src/q6.ts
+++
@@ -1,9 +0,0 @@
-class Q6_Book {
-  constructor(public title: string, public author: string, public year: number) {}
-}
-
-// Demo
-(() => {
-  const b = new Q6_Book("Clean Code", "Robert C. Martin", 2008);
-  console.log(b);
-})();
